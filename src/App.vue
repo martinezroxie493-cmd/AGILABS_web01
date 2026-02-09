59fb308096fdec943720903eaf9e859e79217994
Agilabs <martinezroxie493@gmail.com>
2026-02-09 14:23:40 +0800

    Create src/App.vue as the root Vue component for the website layout

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Welcome to AGILABS</h1>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer>
+      <p>&copy; 2026 AGILABS. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+footer {
+  margin-top: 20px;
+}
+</style>
